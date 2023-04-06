90f15f540e1032df90bb58becc6c7fd37b8b4433
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-06 19:28:12 +0600

    footer added

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
+            <div className='py-6 md:text-sm text-xs text-center text-slate-400'>
+                © 2023 BookStore Inc. All rights reserved.
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
