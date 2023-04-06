8d1ac5374fac56462de95bd923117b7003e67bb8
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-06 15:53:53 +0600

    book fade effect added

--- src/components/Books/Book/Book.jsx
+++ src/components/Books/Book/Book.jsx
@@ -4,20 +4,20 @@ const Book = ({ book }) => {
     console.log(book)
     const { image, isbn13, price, title, subtitle, url } = book
     return (
-        <div className='relative transition duration-300 transform hover:-translate-y-2'>
+        <div className='book-div'>
             {/* <img className='mx-auto' src={image} />
             <h1>{title}</h1> */}
-            <div className="overflow-hidden  border-slate-400 shadow-sm shadow-orange-300  rounded-md">
+            <div className="book-card">
                 <figure className="px-2 w-auto">
-                    <img src={image} className=" object-cover w-full h-56 md:h-64 xl:h-80" />
+                    <img src={image} className="book-card-img" />
                 </figure>
                 <div className="items-center text-center px-2">
                     {/* <h2 className="text-lg pb-3 font-semibold" >{title}</h2> */}
-                    
+
                 </div>
             </div>
 
-            <div className='text-center bg-slate-500 bg-opacity-90 opacity-0 space-y-5  my-auto text-orange-300 font-semibold absolute inset-0 hover:opacity-100 transition duration-500'>
+            <div className='book-fade-effect'>
                 <h1>{title}</h1>
                 <h3>{subtitle}</h3>
                 <p>Price : {price}</p>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -10,7 +10,7 @@ const Books = () => {
     return (
         <div className='my-container'>
             <h1>Total books : {books.length}</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 mb-5 mt-5 p-5'>
                 {
                     books.map(book => <Book book={book} key={book.isbn13}></Book>)
                 }
