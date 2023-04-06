c09da20ec54d7bf96ee7a516405cc362cb904053
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-06 19:30:53 +0600

    loader fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from './components/footer';
 import Header from './components/Header/Header';
+import LoadingSpinner from './components/LoadingSpinner';
 
 const App = () => {
+  // spinner implements 
+  const navigation = useNavigation();
+  if (navigation.state === 'loading') {
+    return <LoadingSpinner></LoadingSpinner>
+  }
   return (
     <div className='container mx-auto'>
       <Header></Header>
